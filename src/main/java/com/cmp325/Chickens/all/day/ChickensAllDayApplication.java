e7202b24bd4bdc987e3528d304af724e6b59b2f5
manlikeayei <eyongayei@gmail.com>
2025-01-19 21:54:51 +0100

    first commit

---
+++ src/main/java/com/cmp325/Chickens/all/day/ChickensAllDayApplication.java
@@ -0,0 +1,13 @@
+package com.cmp325.Chickens.all.day;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChickensAllDayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChickensAllDayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cmp325/Chickens/all/day/ChickensAllDayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmp325.Chickens.all.day;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChickensAllDayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
